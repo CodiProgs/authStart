621f5ae276509ef336837a4ef6f7424ed09b3bf9
CodiProgs <samuil.codiWork@mail.ru>
2023-11-28 13:31:50 +0300

    Added closing of the modal window by timeout

--- client/src/layout/AuthGuard.tsx
+++ client/src/layout/AuthGuard.tsx
@@ -4,10 +4,7 @@ import { ShowSessionQuery } from '@/gql/graphql';
 import { SHOW_SESSION } from '@/graphql/queries/ShowSession';
 import { useUserStore } from '@/stores/userStore';
 import { useQuery } from '@apollo/client';
-import { getCookie } from 'cookies-next';
-import cookie from 'js-cookie'
 import Link from 'next/link';
-import { NextResponse } from 'next/server';
 import React, { useEffect } from 'react';
 import { IoCloseOutline } from 'react-icons/io5';
 
@@ -20,27 +17,44 @@ function AuthGuard ({ children }: { children: React.ReactNode }) {
             userId: user.id
         }
     });
-    
+    const [hover, setHover] = React.useState(false);
+    const [timeoutId, setTimeoutId] = React.useState<NodeJS.Timeout | null>(null);
+
     useEffect(() => {
-        
         const listenStorageChange = () => {
             if(user.id === undefined) {
-                setShow(true)
-            }
+                setShow(true);
+            }            
         };
+        if(show === true){
+            if(timeoutId === null){
+                setTimeoutId(setTimeout(() => {
+                    setShow(false);
+                }, 4000))
+            }
+    
+            if(hover === true){
+                clearTimeout(timeoutId!)
+            }else if(timeoutId !== null){
+                setTimeoutId(setTimeout(() => {
+                    setShow(false);
+                }, 2000))
+            }
+        }
+
         window.addEventListener("Unauthorized", listenStorageChange);
         return () => window.removeEventListener("Unauthorized", listenStorageChange);
-    }, [])
+    }, [user.id, setHover, hover, show, setShow]);
     return (
         <>
             {children}
             {show && (
-                <div className='fixed top-[30px] left-0 right-0 mx-auto max-w-[300px] w-full px-[30px] py-[20px] h-max min-h-[100px] border border-blueColor z-50 flex items-center flex-col gap-4 rounded-lg'>
+                <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} className='fixed bg-white top-[30px] left-0 right-0 mx-auto max-w-[300px] w-full px-[30px] py-[20px] h-max min-h-[100px] border border-blueColor z-50 flex items-center flex-col gap-4 rounded-lg'>
                     <button className='group absolute right-5 top-5 p-1' onClick={() => setShow(!show)}>
                         <IoCloseOutline size={20} className="stroke-[#999999] group-hover:stroke-blueColor" />
                     </button>
                     <p>Вы не авторизованы</p>
-                    <Link href={'/auth/login'} className={['rounded-md h-[34px] bg-colorBackground text-[15px] px-[19px] w-max flex items-center hover:shadow-[0_0_0_2px_#0332FF] font-bold', ''].join(' ')}>
+                    <Link href={'/auth/login'} className={['rounded-md h-[34px] bg-colorBackground text-[15px] px-[19px] w-max flex items-center hover:shadow-[0_0_0_2px_#0332FF] font-bold', ''].join(' ')} onClick={() => setShow(!show)}>
                         Войти
                     </Link>
                 </div>
